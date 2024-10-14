f40083d86571f22b88ef48d0da71ce4e228359ed
Irina Bogomolova <168665027+samka-bogomola-02@users.noreply.github.com>
2024-10-14 13:27:10 +0300

    initial commit

---
+++ src/main/java/ka/irissin/coursework2springdemo/CourseWork2SpringDemoApplication.java
@@ -0,0 +1,12 @@
+package ka.irissin.coursework2springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2SpringDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2SpringDemoApplication.class, args);
+	}
+
+}
